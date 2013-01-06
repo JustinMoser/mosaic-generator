f6bb4a36f8e3482d4ccfc2873b42af9ada149f0b
Justin Moser <justin@baysickdesign.com>
2013-01-06 15:05:02 +0000

    initial service integration commit. Skeleton web service created.

---
+++ Mosaic.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mosaic.Service.Global" Language="C#" %>
---
+++ Mosaic.Service/MosaicService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Mosaic.Service
+{
+    public class MosaicService : IMosaicService
+    {
+        public string GetData(string url)
+        {
+            return url;
+        }
+    }
+}
